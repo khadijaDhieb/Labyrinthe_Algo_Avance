7c8eb0e81c0946a9d629260fe4ab4441d08e43f3
Khadija dh <khadija.dhieb@le-campus-numerique.fr>
2020-12-02 15:04:33 +0100

    modif fonction new_labyrinthe : creation du table arrayLab

--- app.js
+++ app.js
@@ -1,39 +1,32 @@
+let arrayLab;
+
 function randomOrNot() {
     let random = true;
+    let content = document.getElementById('grid-container');
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
     if (random) {
-        randomLab();
+        const randomCase = Math.floor(Math.random() * (Object.keys(labyrinthes).length) + 3);
+        const randomEx = Math.floor(Math.random() * 3);
+        new_labyrinthe(randomCase, labyrinthes[randomCase]["ex-" + randomEx]);
     } else {
-        chooseLab()
+        new_labyrinthe(6, labyrinthes["6"]["ex-0"]);
     }
 }
 
-function randomLab() {
-    let randomCase = Math.floor(Math.random() * Object.keys(labyrinthes).length + 1);
-    let randomEx = Math.floor(Math.random() * 2 + 1);
-    new_labyrinthe(randomCase, labyrinthes[randomCase]["ex-" + randomEx])
-}
-
-function chooseLab() {
-    new_labyrinthe(4, labyrinthes["4"]["ex-0"]);
-}
+/*===============CODE GENERANT LE LABYRINTHE=========================*/
 
 function new_labyrinthe(taille, ex) {
+    arrayLab = new Array(ex.length);
 
-    //effacer le labyrinthe 
-    let div = document.getElementById('grid-container');
-    while (div.firstChild) {
-        div.removeChild(div.firstChild);
-    }
+    document.getElementById("grid-container").style.gridTemplateColumns = "repeat(" + taille + ", 50px)";
+    document.getElementById("grid-container").style.gridTemplateRows = "repeat(" + taille + ", 50px)";
 
-    // recréer le labyrinthe 
-    document.getElementById("grid-container").style.gridTemplateColumns = "repeat(" + taille + ", 100px)";
-    document.getElementById("grid-container").style.gridTemplateRows = "repeat(" + taille + ", 100px)";
-
-
-    //haut droit bas gauche 
+    //haut droite bas gauche
     for (let i = 0; i < ex.length; i++) {
         let borderstyle = "";
-
+        arrayLab[i] = {};
         for (let j = 0; j < ex[i]["walls"].length; j++) {
 
             if (ex[i]["walls"][j]) {
@@ -42,16 +35,18 @@ function new_labyrinthe(taille, ex) {
                 borderstyle = borderstyle + "none ";
             }
         }
+        arrayLab[i]["x"] = ex[i]["posX"];
+        arrayLab[i]["y"] = ex[i]["posY"];
+        arrayLab[i]["walls"] = ex[i]["walls"];
+        arrayLab[i]["isVisited"] = false;
 
-        console.log(borderstyle, "celule num " + ex[i]["posX"] + "_" + ex[i]["posY"]);
         let element = document.createElement("DIV");
-        element.id = "cellule_" + ex[i]["posX"] + "_" + ex[i]["posY"];
-        if (i == ex.length - 1) {
+        element.id = "cellule" + ex[i]["posX"] + "_" + ex[i]["posY"];
+        if (i === ex.length - 1) {
             element.style.backgroundColor = "tomato"
         }
         element.style.borderStyle = borderstyle;
-        element.style.borderColor = "rgb(233, 30, 99)";
+        element.style.borderColor = "rgb(210,10,122)";
         document.getElementById("grid-container").appendChild(element);
-
     }
 }
